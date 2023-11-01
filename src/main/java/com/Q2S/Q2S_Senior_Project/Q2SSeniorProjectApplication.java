1df59726ae16829c1c3d0447b23ad311b95cc144
archie1802 <jonesarchie37@gmail.com>
2023-11-01 16:36:16 -0700

    maven set up

---
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Q2SSeniorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Q2S.Q2S_Senior_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q2SSeniorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q2SSeniorProjectApplication.class, args);
+	}
+
+}
