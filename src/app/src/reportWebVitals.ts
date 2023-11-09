32b2415de1a82c5a35fff24142c800c4b678b1b6
archie1802 <jonesarchie37@gmail.com>
2023-11-09 10:07:59 -0800

    fixing lint errors

--- src/app/src/reportWebVitals.ts
+++ src/app/src/reportWebVitals.ts
@@ -1,14 +1,17 @@
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }): void => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    // tslint:disable-next-line:no-floating-promises
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }): void => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch((err) => console.log(err));
   }
 };
 
