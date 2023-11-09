7e1cbb62587a0665a4ab3287148de0dce52f6960
archie1802 <jonesarchie37@gmail.com>
2023-11-09 09:41:56 -0800

    adding eslint and prettier

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -14,8 +12,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
