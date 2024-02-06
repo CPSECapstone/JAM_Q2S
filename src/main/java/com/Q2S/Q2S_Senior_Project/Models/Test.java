1485c82fc3800b7a383a0953e9a89f9c000e519d
archie1802 <jonesarchie37@gmail.com>
2024-02-05 21:32:31 -0800

    updated application.properties but getting bug not sure why

---
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Models/Test.java
@@ -0,0 +1,4 @@
+package com.Q2S.Q2S_Senior_Project.Models;
+
+public class Test {
+}
--- src/main/java/com/Q2S/Q2S_Senior_Project/Q2SSeniorProjectApplication.java
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Q2SSeniorProjectApplication.java
@@ -2,6 +2,10 @@ package com.Q2S.Q2S_Senior_Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Q2SSeniorProjectApplication {
