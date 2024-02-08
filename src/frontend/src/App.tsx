4e7e774c968278573a16c762d5e84c35a678a5f2
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-02-07 20:19:45 -0800

    JAM-157 #time 3h attempting to adjust the layout of the sideBarItem css

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -7,6 +7,7 @@ import SideBar from "./Componenets/SideBar";
 function App() {
   return (
     <div className="App">
+      <SideBar></SideBar>
       <Grid></Grid>
     </div>
   );
--- src/frontend/src/Componenets/SideBar.tsx
+++ src/frontend/src/Componenets/SideBar.tsx
@@ -8,24 +8,19 @@ function SideBarTab () : JSX.Element {
             <div className="sideBarItems">
                 <div className="sideBarGroup">
                     <span className="sideBarTitle"> Main Flowchart </span>
-                    <ul className="sideBarList">
-                        <SideBarItem id={BigInt(11)} name={"Main Flowchart"}/>
-                    </ul>
+                    <SideBarItem id={BigInt(11)} name={"Main Flowchart"}/>
+
                 </div>
                 <div className="sideBarGroup">
                     <span className="sideBarTitle"> Favorites </span>
-                    <ul className="sideBarList">
-                        <SideBarItem id={BigInt(121)} name={"Favorites 1"}/>
-                        <SideBarItem id={BigInt(122)} name={"Favorites 2"}/>
-                    </ul>
+                    <SideBarItem id={BigInt(121)} name={"Favorites 1"}/>
+                    <SideBarItem id={BigInt(122)} name={"Favorites 2"}/>
                 </div>
                 <div className="sideBarGroup">
                     <span className="sideBarTitle"> All Flowcharts </span>
-                    <ul className="sideBarList">
                     <SideBarItem id={BigInt(1231)} name={"All Flowcharts 1"}/>
                     <SideBarItem id={BigInt(1232)} name={"All Flowcharts 2"}/>
-                    <SideBarItem id={BigInt(1233)} name={"All Flowcharts 3 Extra Long Name"}/>
-                    </ul>
+                    <SideBarItem id={BigInt(1233)} name={"All Flowcharts 3 Extra LongExtraLongExtra Long Name"}/>
                 </div>
             </div>
         </div>
