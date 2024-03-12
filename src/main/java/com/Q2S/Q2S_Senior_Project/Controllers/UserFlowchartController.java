0e15ccb242e2440006bfb53efce30230ba7d70e1
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-03-12 10:20:24 -0700

    initial input checking

--- src/main/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartController.java
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartController.java
@@ -1,16 +1,57 @@
 package com.Q2S.Q2S_Senior_Project.Controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 public class UserFlowchartController {
+
+    private final int semesterTransitionYear = 2026;
+
+    private enum QuarterTerms{
+        Winter,
+        Spring,
+        Summer,
+        Fall
+    }
+
+    private enum SemesterTerms{
+        Spring,
+        Summer,
+        Fall
+    }
 
     /**
      *
      *
      *
      * @param termAdmitted          Assumes the format "<Term> <Year>"
-     * @param flowchartTemplate     String of the appropriate template
+     * @param flowchartTemplate     JSON String of the appropriate template
      * @return   String of the template personalized to the user
      */
-    static String createNewUserQuarterFlowchart(String termAdmitted, String flowchartTemplate){
+    static String createNewUserQuarterFlowchart(String termAdmitted, String flowchartTemplate) throws IOException {
+        String[] admitTermAndYear;
+        int admitYear;
+        JsonNode terms;
+        try {
+            admitTermAndYear = termAdmitted.split(" ");
+            if (admitTermAndYear.length != 2){
+                throw new IOException("Term Admitted has an invalid format. Required: <Term> <Year>. Given: " + termAdmitted);
+            }
+            admitYear = Integer.parseInt(admitTermAndYear[1]);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(flowchartTemplate);
+            terms = jsonNode.get("termData");
+            System.out.println(terms);
+        } catch (NumberFormatException e){
+            throw new IOException("Term Admitted has an invalid year format - " + termAdmitted);
+        } catch (JsonProcessingException e){
+            throw new IOException("Flowchart is in invalid JSON format.");
+        }
+
         return null;
     }
 }
--- src/test/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartControllerTest.java
+++ src/test/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartControllerTest.java
@@ -15,6 +15,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserFlowchartControllerTest {
 
     @Test
+    void createNewUserQuarterFlowchart() throws IOException {
+        UserFlowchartController.createNewUserQuarterFlowchart("Spring 2026", "{\"termData\": \"hello\"}");
+    }
+
+    @Test
     void createNewUserQuarterFlowchart_Spring2026() throws IOException {
         File testJSONFile = new File("src/test/testJSONs/testFlowchart.json");
         String resultingFlowchart = UserFlowchartController.createNewUserQuarterFlowchart(
@@ -120,6 +125,44 @@ class UserFlowchartControllerTest {
                     case 3:
                         assertEquals("Semester", termType);
                         assertEquals("Summer 2026", termName);
+                        break;
+                }
+            }
+        } else {
+            throw new IOException("The \"termData\" field does not contain an array.");
+        }
+    }
+
+    @Test
+    void createNewUserQuarterFlowchart_Spring2027() throws IOException {
+        File testJSONFile = new File("src/test/testJSONs/testFlowchart.json");
+        String resultingFlowchart = UserFlowchartController.createNewUserQuarterFlowchart(
+                "Spring 2027",
+                new String(Files.readAllBytes(testJSONFile.toPath())));
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(resultingFlowchart);
+        JsonNode terms = jsonNode.get("termData");
+        if (terms.isArray()){
+            for (int i = 0; i < terms.size(); i++){
+                JsonNode term = terms.get(i);
+                String termType = term.get("termType").asText();
+                String termName = term.get("termName").asText();
+                switch (i) {
+                    case 0:
+                        assertEquals("Semester", termType);
+                        assertEquals("Fall 2026", termName);
+                        break;
+                    case 1:
+                        assertEquals("Semester", termType);
+                        assertEquals("Spring 2027", termName);
+                        break;
+                    case 2:
+                        assertEquals("Semester", termType);
+                        assertEquals("Summer 2027", termName);
+                        break;
+                    case 3:
+                        assertEquals("Semester", termType);
+                        assertEquals("Fall 2027", termName);
                         break;
                 }
             }
