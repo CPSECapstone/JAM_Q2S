90c8ea0cdc68dfd25980f02604e8237c113ff17b
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-03-12 18:51:27 -0700

    JAM-203 #time 3h implementing and testing functionality for making a template specific to a user's start term

--- src/main/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartController.java
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartController.java
@@ -3,24 +3,27 @@ package com.Q2S.Q2S_Senior_Project.Controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
 
 public class UserFlowchartController {
 
-    private final int semesterTransitionYear = 2026;
+    private static final int SEMESTER_TRANSITION_YEAR = 2026;
 
-    private enum QuarterTerms{
-        Winter,
+    private static final String QUARTER_TAG = "Quarter";
+    private static final String SEMESTER_TAG = "Semester";
+
+    private static final int QUARTER_MOD_NUMBER = 4;
+    private static final int SEMESTER_MOD_NUMBER = 3;
+
+    // % 4 for Quarter, % 3 for Semester
+    enum TermSeason {
         Spring,
         Summer,
-        Fall
-    }
-
-    private enum SemesterTerms{
-        Spring,
-        Summer,
-        Fall
+        Fall,
+        Winter
     }
 
     /**
@@ -32,26 +35,108 @@ public class UserFlowchartController {
      * @return   String of the template personalized to the user
      */
     static String createNewUserQuarterFlowchart(String termAdmitted, String flowchartTemplate) throws IOException {
-        String[] admitTermAndYear;
-        int admitYear;
-        JsonNode terms;
-        try {
-            admitTermAndYear = termAdmitted.split(" ");
-            if (admitTermAndYear.length != 2){
-                throw new IOException("Term Admitted has an invalid format. Required: <Term> <Year>. Given: " + termAdmitted);
+        ObjectMapper mapper = new ObjectMapper();
+        int[] intInfo = getValidatedTermAdmittedYearAndOrdinal(termAdmitted);
+        int year = intInfo[0];
+        JsonNode rootNode = getValidatedFlowchartTemplateInfo(flowchartTemplate);
+        ArrayNode terms = (ArrayNode) rootNode.get("termData");
+        int termSeasonIterator = intInfo[1] -1; //iterate from one term before when the students started
+        int offset = 0;
+        String termType = QUARTER_TAG;
+        int enumModNumber = QUARTER_MOD_NUMBER;
+        int size = terms.size();
+        for (int i = 0; i < size; i++) {
+            int index = i + offset;
+            TermSeason termSeason = TermSeason.values()[(enumModNumber + termSeasonIterator) % enumModNumber];
+            // if this is the first instance of a semester term - change to semesters
+            // should happen when Fall 2026 is reached
+            if (termType.equals(QUARTER_TAG) && !isQuarterTerm(termSeason, year)){
+                enumModNumber = SEMESTER_MOD_NUMBER;
+                termType = SEMESTER_TAG;
             }
-            admitYear = Integer.parseInt(admitTermAndYear[1]);
+            ObjectNode node = (ObjectNode) terms.get(index);
+            node.put("termName", termSeason.name() + " " + year);
+            node.put("termType", termType);
+            if (termSeason == TermSeason.Spring){
+                index++;
+                ObjectNode newObject = mapper.createObjectNode();
+                newObject.put("termName", TermSeason.Summer + " " + year);
+                newObject.put("termType", termType);
+                newObject.put("courses", "[]");
+                terms.insert(index,newObject);
+                termSeasonIterator++;
+                offset++;
+            }
+            if (termSeason == TermSeason.Fall){
+                year++;
+            }
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(flowchartTemplate);
-            terms = jsonNode.get("termData");
-            System.out.println(terms);
-        } catch (NumberFormatException e){
-            throw new IOException("Term Admitted has an invalid year format - " + termAdmitted);
-        } catch (JsonProcessingException e){
-            throw new IOException("Flowchart is in invalid JSON format.");
+            termSeasonIterator++;
         }
 
-        return null;
+        String updatedJsonString = mapper.writeValueAsString(rootNode);
+        System.out.println(updatedJsonString);
+        return updatedJsonString;
+    }
+
+    static boolean isQuarterTerm(TermSeason season, int year){
+        return year < SEMESTER_TRANSITION_YEAR || (year == SEMESTER_TRANSITION_YEAR & season != TermSeason.Fall);
+    }
+
+    /**
+     * Validates that the term admitted data is of the correct format and
+     * returns corresponding integer information
+     *
+     * @param termAdmitted  Term Admitted string
+     * @return                   Array containing the integer values of the start year
+     *                              and the starting ordinal for iterating
+     */
+    public static int[] getValidatedTermAdmittedYearAndOrdinal(String termAdmitted){
+        String[] splitTermAdmitted = termAdmitted.split(" ");
+        int admitYear;
+        try {
+            if (splitTermAdmitted.length != 2) {
+                throw new IllegalStateException("Term Admitted has an invalid format. Required: <Term> <Year>. Given: " + termAdmitted);
+            }
+            admitYear = Integer.parseInt(splitTermAdmitted[1]);
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("Term Admitted has an invalid year format. Required: <YYYY>. Given: " + splitTermAdmitted[1]);
+        }
+        return new int[] {admitYear, getStartingOrdinal(splitTermAdmitted[0])};
+    }
+
+    /**
+     *
+     * @param flowchartTemplate   String of Flowchart template for major, concentration, and catalog chosen
+     * @return                    Flowchart personalized to the termAdmitted submitted
+     */
+    private static JsonNode getValidatedFlowchartTemplateInfo(String flowchartTemplate){
+        JsonNode rootNode;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            rootNode = objectMapper.readTree(flowchartTemplate);
+            JsonNode terms = rootNode.get("termData");
+            if (!terms.isArray()){
+                throw new IllegalStateException("\"termData\" field is improperly formatted. It should be an array.");
+            }
+        } catch (JsonProcessingException e){
+            throw new IllegalStateException("Flowchart is in invalid JSON format.");
+        }
+        return rootNode;
+    }
+
+    /**
+     *
+     * @param startQuarterTerm  - Quarter Term Season of Admit Term - Should be Winter, Spring, Summer, or Fall
+     * @return  - the ordinal of the corresponding term Enum for iteration purposes
+     */
+    private static int getStartingOrdinal(String startQuarterTerm) {
+        return switch (startQuarterTerm.toLowerCase()) {
+            case "winter" -> TermSeason.Winter.ordinal();
+            case "spring" -> TermSeason.Spring.ordinal();
+            case "summer" -> TermSeason.Summer.ordinal();
+            case "fall" -> TermSeason.Fall.ordinal();
+            default -> throw new IllegalStateException("Term must be Winter, Spring, Summer, or Fall.Unexpected value: " + startQuarterTerm.toLowerCase());
+        };
     }
 }
--- src/test/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartControllerTest.java
+++ src/test/java/com/Q2S/Q2S_Senior_Project/Controllers/UserFlowchartControllerTest.java
@@ -2,21 +2,117 @@ package com.Q2S.Q2S_Senior_Project.Controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserFlowchartControllerTest {
 
     @Test
-    void createNewUserQuarterFlowchart() throws IOException {
-        UserFlowchartController.createNewUserQuarterFlowchart("Spring 2026", "{\"termData\": \"hello\"}");
+    void testIsQuarterTerm_Fall2026() {
+        assertFalse(UserFlowchartController.isQuarterTerm(UserFlowchartController.TermSeason.Fall, 2026));
+    }
+
+    @Test
+    void testIsQuarterTerm_Spring2026() {
+        assertTrue(UserFlowchartController.isQuarterTerm(UserFlowchartController.TermSeason.Spring, 2026));
+    }
+
+    @Test
+    void testIsQuarterTerm_Spring2027() {
+        assertFalse(UserFlowchartController.isQuarterTerm(UserFlowchartController.TermSeason.Spring, 2027));
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the termAdmitted is not properly formatted
+     * - Only one word
+     */
+    @Test
+    void getValidatedTermAdmittedYearAndOrdinal_InvalidTermAdmitted_Length1() {
+        Exception exception = assertThrows(IllegalStateException.class,
+                () -> UserFlowchartController.getValidatedTermAdmittedYearAndOrdinal("2026"));
+
+        String expectedMessage = "Term Admitted has an invalid format. Required: <Term> <Year>.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the termAdmitted is not properly formatted
+     * - More than two words
+     */
+    @Test
+    void getValidatedTermAdmittedYearAndOrdinal_InvalidTermAdmitted_Length3() {
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+            UserFlowchartController.getValidatedTermAdmittedYearAndOrdinal("Spring 2026 V2"));
+
+        String expectedMessage = "Term Admitted has an invalid format. Required: <Term> <Year>.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the termAdmitted is not properly formatted
+     * - Invalid term season (Not winter, spring, summer, or fall)
+     */
+    @Test
+    void getValidatedTermAdmittedYearAndOrdinal_InvalidTermAdmitted_InvalidSeason() {
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                UserFlowchartController.getValidatedTermAdmittedYearAndOrdinal("Unknown 2026"));
+
+        String expectedMessage = "Term must be Winter, Spring, Summer, or Fall.Unexpected value";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the termAdmitted is not properly formatted
+     * - Year not parseable into Int
+     */
+    @Test
+    void getValidatedTermAdmittedYearAndOrdinal_InvalidYearAdmitted() {
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                UserFlowchartController.getValidatedTermAdmittedYearAndOrdinal("Spring 20v6"));
+
+        String expectedMessage = "Term Admitted has an invalid year format. Required: <YYYY>.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the flowchartTemplate is not properly formatted
+     * - Invalid JSON format
+     */
+    @Test
+    void createNewUserQuarterFlowchart_InvalidFlowchart_InvalidJSON() {
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                UserFlowchartController.createNewUserQuarterFlowchart("Spring 2026", "hello"));
+
+        String expectedMessage = "Flowchart is in invalid JSON format.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage,actualMessage);
+    }
+
+    /**
+     * Tests that the proper exception is thrown when the flowchartTemplate is not properly formatted
+     * - termData does not hold an array as expected
+     */
+    @Test
+    void createNewUserQuarterFlowchart_InvalidFlowchart_InvalidTermData() {
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                UserFlowchartController.createNewUserQuarterFlowchart("Spring 2026", "{\"termData\": \"hello\"}"));
+
+        String expectedMessage = "\"termData\" field is improperly formatted. It should be an array.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage,actualMessage);
     }
 
     @Test
@@ -53,7 +149,7 @@ class UserFlowchartControllerTest {
                 }
             }
         } else {
-            throw new IOException("The \"termData\" field does not contain an array.");
+            throw new IllegalStateException("The \"termData\" field does not contain an array.");
         }
     }
 
@@ -86,12 +182,12 @@ class UserFlowchartControllerTest {
                         break;
                     case 3:
                         assertEquals("Semester", termType);
-                        assertEquals("Spring 2026", termName);
+                        assertEquals("Spring 2027", termName);
                         break;
                 }
             }
         } else {
-            throw new IOException("The \"termData\" field does not contain an array.");
+            throw new IllegalStateException("The \"termData\" field does not contain an array.");
         }
     }
 
@@ -120,49 +216,11 @@ class UserFlowchartControllerTest {
                         break;
                     case 2:
                         assertEquals("Semester", termType);
-                        assertEquals("Spring 2026", termName);
-                        break;
-                    case 3:
-                        assertEquals("Semester", termType);
-                        assertEquals("Summer 2026", termName);
-                        break;
-                }
-            }
-        } else {
-            throw new IOException("The \"termData\" field does not contain an array.");
-        }
-    }
-
-    @Test
-    void createNewUserQuarterFlowchart_Spring2027() throws IOException {
-        File testJSONFile = new File("src/test/testJSONs/testFlowchart.json");
-        String resultingFlowchart = UserFlowchartController.createNewUserQuarterFlowchart(
-                "Spring 2027",
-                new String(Files.readAllBytes(testJSONFile.toPath())));
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(resultingFlowchart);
-        JsonNode terms = jsonNode.get("termData");
-        if (terms.isArray()){
-            for (int i = 0; i < terms.size(); i++){
-                JsonNode term = terms.get(i);
-                String termType = term.get("termType").asText();
-                String termName = term.get("termName").asText();
-                switch (i) {
-                    case 0:
-                        assertEquals("Semester", termType);
-                        assertEquals("Fall 2026", termName);
-                        break;
-                    case 1:
-                        assertEquals("Semester", termType);
                         assertEquals("Spring 2027", termName);
                         break;
-                    case 2:
-                        assertEquals("Semester", termType);
-                        assertEquals("Summer 2027", termName);
-                        break;
                     case 3:
                         assertEquals("Semester", termType);
-                        assertEquals("Fall 2027", termName);
+                        assertEquals("Summer 2027", termName);
                         break;
                 }
             }
