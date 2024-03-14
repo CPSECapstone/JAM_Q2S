d406c1e83ebd113b91e264bc82658ac0f5d807c9
archie1802 <jonesarchie37@gmail.com>
2024-03-14 09:39:50 -0700

    temp

--- src/frontend/src/Components/TSX/Class.tsx
+++ src/frontend/src/Components/TSX/Class.tsx
@@ -1,68 +1,88 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../CSS/Class.css';
-import { Draggable } from '@hello-pangea/dnd';
-import { StyledClass } from '../StyledComponents/ClassStyles';
+import {Draggable} from '@hello-pangea/dnd';
+import {StyledClass} from '../StyledComponents/ClassStyles';
 import {ClassDBClass, EmbeddedSemesterClassData, QuarterClassData} from '../../Interfaces/Interfaces';
 import EmbeddedClass from "./EmbeddedClass";
-
-
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import {Tooltip} from "react-tooltip";
 
 interface classProps {
-  classData: ClassDBClass;
-  index: number;
-  handleRightClick: (classId: string, x: number, y: number) => void;
+    classData: ClassDBClass;
+    index: number;
+    handleRightClick: (classId: string, x: number, y: number) => void;
 }
 
 const mockData: EmbeddedSemesterClassData = {
-  id: "CSC 1001",
-  displayName: "Introduction to Computer Science",
-  units: "4"
+    id: "CSC 1001",
+    displayName: "Introduction to Computer Science",
+    units: "4"
 };
 
+const tooltipStyles = {
+    'background': '#2c372d',
+    'maxWidth': '300px',
+    'zIndex': '999'
+}
 
-function Class({ index, classData, handleRightClick }: classProps) {
-  const data: QuarterClassData = classData.classData;
-  const [isEmbeddedClassOpen, setEmbeddedClassOpen] = useState<boolean>(false);
+function Class({index, classData, handleRightClick}: classProps) {
+    const data: QuarterClassData = classData.classData;
+    const [isEmbeddedClassOpen, setEmbeddedClassOpen] = useState<boolean>(false);
 
-  const toggleEmbeddedClass = () => {
-    setEmbeddedClassOpen(!isEmbeddedClassOpen);
-  };
+    const toggleEmbeddedClass = () => {
+        setEmbeddedClassOpen(!isEmbeddedClassOpen);
+    };
 
-  return (
-      <Draggable draggableId={data.id}
-                 index={index}
-                 key={data.id}>
-        {(provided) => (
-            <StyledClass
-              id={data.id}
-              {...provided.dragHandleProps}
-              ref={provided.innerRef}
-              {...provided.draggableProps} style={{ ...provided.draggableProps.style }}
-              color={classData.color}
-              $expanded = {isEmbeddedClassOpen}
-              onContextMenu={(e) => {
-                e.preventDefault()
-                handleRightClick(data.id, e.pageX, e.pageY)
-              }}
-              onClick={toggleEmbeddedClass}>
-
-              <div className='courseCode'>
-                <p>{data.id + ' (' + data.units + ')'}</p>
-              </div>
-              <div className='courseName'>
-                <p>{data.displayName}</p>
-              </div>
-              <div className="collapsible">
-                <p>{isEmbeddedClassOpen ? '▲' : '▼'}</p>
-              </div>
-              {isEmbeddedClassOpen && (
-                  <div className="embedded-class">
-                    <EmbeddedClass data={mockData}/>
-                  </div>
-              )}
-            </StyledClass>)}
-      </Draggable>
-  );
+    return (
+        <Draggable draggableId={data.id}
+                   index={index}
+                   key={data.id}>
+            {(provided) => (
+                <StyledClass
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps} style={{...provided.draggableProps.style}}
+                    color={classData.color}
+                    $expanded={isEmbeddedClassOpen}
+                    onContextMenu={(e) => {
+                        e.preventDefault()
+                        handleRightClick(data.id, e.pageX, e.pageY)
+                    }}
+                    onClick={toggleEmbeddedClass}>
+                    <div className="infoIcon">
+                        <InfoOutlinedIcon fontSize="small" id={data.id}/>
+                    </div>
+                    <div className='courseCode'>
+                        <p>{data.id + ' (' + data.units + ')'}</p>
+                    </div>
+                    <div className='courseName'>
+                        <p>{data.displayName}</p>
+                    </div>
+                    <div className="collapsible">
+                        <p>{isEmbeddedClassOpen ? '▲' : '▼'}</p>
+                    </div>
+                    <Tooltip
+                        anchorSelect={"#" + data.id}
+                        place="right"
+                        className="classInfo"
+                        delayShow={100}
+                        opacity={100}
+                        style={tooltipStyles}>
+                        <b>{data.id + "\n"}</b><br></br>
+                        {data.displayName}<br></br>
+                        <hr></hr>
+                        {data.desc}
+                        <hr></hr>
+                        {data.addl}
+                    </Tooltip>
+                    {isEmbeddedClassOpen && (
+                        <div className="embedded-class">
+                            <EmbeddedClass data={mockData}/>
+                        </div>
+                    )}
+                </StyledClass>)}
+        </Draggable>
+    );
 }
 
 export default Class;
