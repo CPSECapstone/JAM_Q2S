35ba45835316613ad22949f6ea9d40fdcc7a33d9
JennyFerriol <jennyanferriol@gmail.com>
2024-04-11 17:50:02 -0700

    Adding microsoft oauth

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -11,8 +11,27 @@ import React, { useState } from "react";
 import {Link, Link as RouterLink} from "react-router-dom";
 import axios from "axios";
 import './Login.css';
+import {UnauthenticatedTemplate, useMsal} from "@azure/msal-react";
+import {loginRequest} from "../authConfig";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrosoft } from '@fortawesome/free-brands-svg-icons';
 
 const Login = () => {
+    const { instance } = useMsal();
+
+    const handleLoginRedirect = () => {
+        instance
+            .loginRedirect({
+                ...loginRequest,
+                prompt: 'create',
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const handleLogoutRedirect = () => { // need to create button to do this and move to home page
+        instance.logoutRedirect().catch((error) => console.log(error));
+    };
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(false);
@@ -24,8 +43,12 @@ const Login = () => {
                 email: email,
                 password: password,
             });
-            window.location.href = '/home';
-
+            if (response.status === 200 && response.data === "Login successful") {
+                // Redirect to home page
+                window.location.href = '/home';
+            } else {
+                // Handle unsuccessful login [ TO DO ]
+            }
         } catch (error) {
             console.error('Error logging in user:', error);
         }
@@ -44,15 +67,20 @@ const Login = () => {
                        onChange={(event) => setPassword(event.target.value)} required/><br/>
 
                 <button type='submit'>Login</button>
-
-                <Grid container>
-                    <Grid item>
-                        <Link to="/register">Don't have an account? Register</Link>
-                    </Grid>
-                </Grid>
             </form>
+            <hr className="divider"/> {/* Divider with custom class */}
+            <button onClick={handleLoginRedirect}> {/* Adjust variant as needed */}
+                <FontAwesomeIcon icon={faMicrosoft}/>
+                <span className="microsoft-sign-in">Sign in with Microsoft</span>
+            </button>
+            <Grid container>
+                <Grid item>
+                    <Link to="/register">Don't have an account? Register</Link>
+                </Grid>
+            </Grid>
         </div>
     );
 };
+
 
 export default Login;
