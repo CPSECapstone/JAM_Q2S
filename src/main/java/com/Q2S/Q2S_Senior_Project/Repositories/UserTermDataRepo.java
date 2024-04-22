0b9d36d5c5d735731d81b04442f4488e9552b931
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-04-21 22:11:42 -0700

    Added initial API for userTermData - tested

---
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Repositories/UserTermDataRepo.java
@@ -0,0 +1,7 @@
+package com.Q2S.Q2S_Senior_Project.Repositories;
+
+import com.Q2S.Q2S_Senior_Project.Models.UserTermDataModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTermDataRepo extends JpaRepository<UserTermDataModel, Long> {
+}
