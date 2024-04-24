5d30979253b63457ae04ec0b7cc66725041eee15
ARES-79 <89227872+ARES-79@users.noreply.github.com>
2024-04-23 20:42:42 -0700

    JAM-243 #time 30m fixing FetchType.Lazy bug

--- src/main/java/com/Q2S/Q2S_Senior_Project/Models/UserFlowchartModel.java
+++ src/main/java/com/Q2S/Q2S_Senior_Project/Models/UserFlowchartModel.java
@@ -3,6 +3,9 @@ package com.Q2S.Q2S_Senior_Project.Models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Getter
@@ -14,15 +17,19 @@ public class UserFlowchartModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="userId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userId", nullable = false)
+    /*flowchart data will be deleted when its associated user is deleted*/
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
     private UserModel userId;
     private String catalogYear;
     private String Major;
     private String Concentration;
     private boolean isMain;
     private boolean isFavorite;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name="userFlowchartId")
     private UserTermDataModel termDataId;
 }
+
