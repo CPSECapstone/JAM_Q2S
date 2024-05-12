49fa67b3e8f3e7a435a5c46360ccb5a193793acb
mitashi-11 <mitashipari@gmail.com>
2024-05-12 13:55:47 -0700

    Updated About page

--- src/frontend/src/pages/About.tsx
+++ src/frontend/src/pages/About.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react'; // Removed unused imports
+import React, {useEffect} from 'react';
 import '../Components/CSS/About.css';
+import {Link} from "react-router-dom";
 
 const About = () => {
     useEffect(() => {
@@ -7,9 +8,54 @@ const About = () => {
 
     return(
         <div className='About'>
-            <h2>ABOUT POLY PLANNER PRO</h2>
+            <div className="about-section">
+                <h1>About Poly Planner Pro</h1>
+                <div>
+                    <p>Some text about who we are and what we do.</p>
+                    <p>Resize the browser window to see that this page is responsive by the way.</p>
+                </div>
+                <div>
+                    <button className='button'>
+                        <Link style={{color:'white', textDecoration: 'none'}} to="/home">HOME</Link>
+                    </button>
+                </div>
+            </div>
 
-            <hr className="divider"/>
+            <div className="row">
+                <h2 style={{padding: '2%', color: 'white', textAlign: 'center'}}>Our Team</h2>
+                <div className="column">
+                    <div className="card">
+                        <img src="/w3images/team1.jpg" alt="Jane" style={{width: "100%"}} />
+                            <div className="container">
+                                <h2>Jane Doe</h2>
+                                <p className="title">SOFTWARE ENGINEER</p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                            </div>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="card">
+                        <img src="/w3images/team2.jpg" alt="Mike" style={{width: "100%"}}/>
+                            <div className="container">
+                                <h2>Mike Ross</h2>
+                                <p className="title">SOFTWARE ENGINEER</p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                            </div>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="card">
+                        <img src="/w3images/team3.jpg" alt="John" style={{width: "100%"}}/>
+                            <div className="container">
+                                <h2>John Doe</h2>
+                                <p className="title">SOFTWARE ENGINEER</p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
