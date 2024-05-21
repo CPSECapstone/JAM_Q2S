27c30dc28e687185d0c3195f0e27ca4c8cb7cda0
mitashi-11 <mitashipari@gmail.com>
2024-05-21 10:08:46 -0700

    Working commit

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'; // Removed unused imports
+import React, {useContext, useEffect, useState} from 'react'; // Removed unused imports
 import Grid from '../Components/TSX/Grid';
 import '../Components/CSS/Home.css';
 import TopBar from '../Components/TSX/TopBar';
@@ -7,6 +7,7 @@ import {ClassDisplayInformation, FlowchartMetaData, QuarterClassData} from '../I
 import {SideBar} from '../Components/TSX/SideBar';
 import {Loader} from '../Components/TSX/Loader'; // Assuming you have a Loader component
 import {StyledSideBar} from '../Components/StyledComponents/SideBarStyle';
+import {AuthContext} from "../Context/AuthContext";
 
 const Home = () => {
     const [totalUnits, setTotalUnits] = useState<number>(0);
@@ -17,6 +18,8 @@ const Home = () => {
         [classUUID: string]: ClassDisplayInformation
     }>({})
     const [sidebarVisible, setSidebarVisible] = useState<boolean>(false);
+    const { setUser } = useContext(AuthContext);
+
     useEffect(() => {
         const loadClassCache = async () => {
             try {
@@ -31,6 +34,20 @@ const Home = () => {
                 setLoading(false);
             }
         };
+
+        setUser({
+            admit_type: "",
+            catalog_year: "2022-2026",
+            concentration: "Non-Concentration Option",
+            email: "mdparikh@calpoly.edu",
+            first_name: "Mitashi",
+            last_name: "Parikh",
+            major: "Computer Science",
+            minor: "",
+            term_admitted: "Fall 2022",
+            user_id: "2002",
+            user_name: "mdparikh"
+        })
 
         loadClassCache();
     }, []);
